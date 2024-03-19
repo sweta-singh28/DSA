fcf9a5d5fc847e523d935ec3842569d307a579fc
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-19 23:03:27 +0530

    first commit

---
+++ Arrays/Array_address.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //array declaration 
+
+
+    int arr[5];
+    cout<<"The address of the array is: "<< &arr<<endl;
+    cout<<arr<<endl;
+
+    
+    return 0;
+}
