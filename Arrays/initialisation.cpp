22f5a16c854ccbdf1a8a23879938436224b18dc1
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-19 23:42:16 +0530

    sweta

---
+++ Arrays/initialisation.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1, 2, 4, 6, 8};
+    // int brr[5] = {1, 2, 4, 6, 8};
+    // int arr[10] = {1, 2, 4, 6, 8};
+
+    //printing all values 
+    for(int i=0; i<5; i++){
+        cout<<arr[i];
+    }
+
+    
+    
+    return 0;
+}
