2257bf4fc9c1a3e09ebf154e14b46efa10d4b471
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-21 18:23:33 +0530

    sweta

---
+++ Arrays/FindUniqueElement.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int findUniqueElement(vector<int>arr){
+    int ans = 0;
+
+    for (int i = 0; i <arr.size(); i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+    
+
+}
+
+
+
+int main(){ 
+
+    int n;
+    cout<<"Enter the size of array: "<<endl;
+    cin>>n;
+
+    vector<int>arr(n);
+    cout<<"Enter the elements: "<<endl;
+
+    for (int i = 0; i <arr.size(); i++)
+    {
+        cin>>arr[i];
+
+    }
+    int UniqueElement = findUniqueElement(arr);
+    cout<<"Unique element in the array is: "<<UniqueElement<<endl;
+    
+    return 0;
+}
