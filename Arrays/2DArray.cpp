770d212324ce6d69a244a1bdd18bcfb58b746b6d
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-23 23:16:09 +0530

    sweta

---
+++ Arrays/2DArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //declare 2D array
+    int arr[3][3];
+
+
+    //initialisation
+    int brr[3][3] = {
+        {1,4,3},
+        {7,5,3},
+        {8,2,9}};
+
+        cout<<brr[0][0]<<endl;
+        cout<<brr[2][1]<<endl;
+        cout<<brr[2][0]<<endl;
+        cout<<brr[0][2]<<endl;
+        cout<<brr[1][0]<<endl;
+        cout<<brr[1][3]<<endl;
+        cout<<brr[1][3]<<endl;
+
+
+
+
+
+    return 0;
+}
