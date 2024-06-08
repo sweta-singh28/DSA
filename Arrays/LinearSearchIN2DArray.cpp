052741a22ce59dd73d597b2ef561271124783923
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-08 21:45:52 +0530

    sweta

--- Arrays/LinearSearchIN2DArray.cpp
+++ Arrays/LinearSearchIN2DArray.cpp
@@ -1,60 +1,50 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-//SEARCHING AN ELEMENT
-
 bool findKey(int arr[][3], int rows, int cols, int key){
-    for (int i = 0; i < rows; i++){
-        for (int j = 0; j < cols; j++)
-        {
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
             if(arr[i][j] == key){
                 return true;
             }
         }
-        
     }
     return false;
 }
 
 int main(){
-
-    int arr[3][3]; 
-
+    
+    int arr[3][3];
     int rows = 3;
     int cols = 3;
-
-    //taking input
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    
+    int key = 9;
+    
+    //taking input 
+    cout<<"Enter the elements: "<<endl;
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
             cin>>arr[i][j];
         }
-        
     }
-
-    cout<<"Printing the rows and columns: "<<endl;
-    //Printing
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
+    
+    //printing the array
+    cout<<"Printing the array: "<<endl;
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
-        
     }
-
     
-  
-    int key = 3;
-
-    if(cout<<findKey(arr,3,3,key)){
-        cout<<"True"<<endl;
+    if(findKey(arr, rows, cols, key)){
+        cout<<"Present"<<endl;
     }
     else{
-        cout<<"Flase"<<endl;
+        cout<<"Absent"<<endl;
     }
+    
     
     return 0;
 }
