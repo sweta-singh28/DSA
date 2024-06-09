2c26cda7cb27fd1a9a1a05c625f8203d430d6ffb
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-09 11:58:18 +0530

    sweta

--- Arrays/Transposein2DArray.cpp
+++ Arrays/Transposein2DArray.cpp
@@ -1,69 +1,44 @@
-#include<iostream>
-#include<limits.h>
+#include <iostream>
 using namespace std;
 
-void transposeArray(int arr[][3], int rows, int cols){
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            swap(arr[i][j], arr[j][i]);
+void transposeArray(int arr[][3], int rows, int cols, int transpose[][3]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transpose[j][i] = arr[i][j];
         }
-        
     }
 }
 
-
- void printArray(int arr[][3], int rows, int cols){
-         for (int  i = 0; i < rows; i++)
-         {
-            for (int j = 0; j < cols; j++)
-            {
-                cout<<arr[i][j]<<" ";
-            }
-            cout<<endl;
-                
-         }
-         
+void printArray(int arr[][3], int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
-    
+}
 
-int main(){
-
+int main() {
     int arr[3][3];
     int rows = 3;
     int cols = 3;
 
-    //taking input
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            cin>>arr[i][j];
+    // Taking input
+    cout<<"Enter the elements: "<<endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
         }
-        
     }
 
-    cout<<"Printing the array: "<<endl;
-    //printing
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-        
-    }
-
-    //Printing the array
-
-    cout<<"Printing the array: "<<endl;
-
-
-    // printArray(arr, rows, cols);
-    transposeArray(arr, rows, cols);
+    cout << "Printing the array: " << endl;
     printArray(arr, rows, cols);
+
+    int transpose[3][3];
+    transposeArray(arr, rows, cols, transpose);
+
+    cout << "Printing the transposed array: " << endl;
+    printArray(transpose, rows, cols);
 
     return 0;
 }
