75af1a7728874b457fd73d109102f603e5b8d581
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-20 23:32:48 +0530

    string

---
+++ String/string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+int main() {
+    char name[100];
+    
+    cout<<"Enter your name: "<<endl;
+    cin>>name;
+    
+    cout<<"Your name is "<<name<<endl;
+
+
+    return 0;
+}
