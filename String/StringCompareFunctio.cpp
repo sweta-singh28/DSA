9fc1dbd7a88fce7af6b55600f4810fd7c33852dd
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-21 20:17:49 +0530

    sweta

---
+++ String/StringCompareFunctio.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+bool compareString(string a, string b){
+    if(a.length() != b.length()){
+        return false;
+    }
+    
+    for(int i=0; i<a.length(); i++){
+        if(a[i] != b[i]){
+            return false;
+        }
+    }
+    return true;
+    
+}
+
+
+
+int main(){
+
+
+    //comparision of two string
+    //(a.compare(b) ->returns 0 if two string are excatly equal
+    
+    string a = "swetaaa";
+    string b = "singhhh";
+    
+    if(compareString(a, b) == false){
+        cout<<"Both the strings are not exactly sane.";
+    }
+    else{
+        cout<<"Both the string are exactly same";
+    }
+    
+
+
+
+    
+    return 0;
+}
