03adb82f9caace711232907c8e7e79d051be7c43
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-21 20:05:29 +0530

    sweta

--- String/string.cpp
+++ String/string.cpp
@@ -1,28 +1,14 @@
-#include <iostream>
-#include<vector>
+#include<iostream>
 #include<string>
 using namespace std;
-
-int main() {
-    char name[100];
-    //taking the entire char sequence as an input
-    cout<<"Enter your name: "<<endl;
-    cin>>name;
-    
-    cout<<"Your name is "<<name<<endl;
-
-
-    //taking single input
-    char ch[100];
-
-    ch[0] = 's';
-    ch[1] = 'w';
-    ch[2] = 'e';
-    ch[3] = 't';
-    cin>>ch[4];
-
-    cout<<ch[0]<<ch[1]<<ch[2]<<ch[3]<<ch[4];
-
-
+int main(){
+     //string create
+    string str;
+    //input
+    cin>>str;
+    //output
+    //to take space as an input
+    //getline(cin, str);
+    //cout<<str<<endl;a
     return 0;
 }
