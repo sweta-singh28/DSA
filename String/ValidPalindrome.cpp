7b9c154c9498782f5f3a0698640e640b73e23d82
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-22 20:53:10 +0530

    sweta

---
+++ String/ValidPalindrome.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+bool checkPalindrome(string s, int i, int j){
+    while(i<=j){
+        if(s[i] != s[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+    
+}
+bool validPalindrome(string s){
+    int i = 0;
+    int j = s.length()-1;
+    while(i<=j){
+        if(s[i] != s[j]){
+            //remove either i or j
+            return checkPalindrome(s, i+1, j) || checkPalindrome(s, i, j-1);
+        }
+        else{
+            //s[i] == s[j]
+            i++;
+            j--;
+        }
+    }
+    return true;
+    
+}
+
+
+
+int main(){
+    //Valind palindrome
+    string s = "aba";
+    
+    cout<<validPalindrome(s)<<endl;
+    
+    
+    
+    
+    return 0;
+}
