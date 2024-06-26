8043221f14a67bc430e7e8bf6674972728bcd537
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-26 20:36:11 +0530

    pointers

---
+++ Pointers/SizeOfPointers.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  //size of pointers
+
+  int a = 5;
+  int*p = &a;
+  cout<<sizeof(p)<<endl;
+
+  char ch = 'b';
+  char*c = &ch;
+  cout<<sizeof(c)<<endl;
+
+  double d = 5.0;
+  double*dtr = &d;
+  cout<<sizeof(dtr)<<endl;
+
+  
+
+  
+
+
+
+
+
+  
+  return 0;
+}
