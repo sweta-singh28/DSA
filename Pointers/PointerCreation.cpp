fbc0077d9e8f2a920b0226aba2e905561fcc89db
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-26 19:24:46 +0530

    pointers

---
+++ Pointers/PointerCreation.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  //int a = 5;
+  //int b = 5;
+  //cout<<a<<endl;
+
+
+//address of the stroge location where the 5 is stored in symbol table
+  //cout<<&a<<endl;
+  //cout<<&b<<endl;
+
+
+  
+  int a = 5;
+  //pointer create
+  //ptr is pointer to a which contain integer data
+  int *ptr = &a;
+
+  //acess the value ptr is pointing to
+  cout<<*ptr<<endl; //using *ptr as dereference 
+
+
+
+
+
+
+  
+  return 0;
+}
