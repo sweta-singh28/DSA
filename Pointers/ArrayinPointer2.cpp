1ec9fd69a2f3d30306da22c4c64aec70c9ecc0ee
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-29 23:10:47 +0530

    sweta

---
+++ Pointers/ArrayinPointer2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int arr[10];
+  cout<<sizeof(arr)<<endl;
+
+  //impt
+
+  int *p = arr;
+  cout<<sizeof(p)<<endl;
+
+  cout<<sizeof(*p)<<endl;
+
+
+  return 0;
+}
