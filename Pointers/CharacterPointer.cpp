42a9c600552c1df54f7915bb889b36ba3158f486
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-29 23:11:02 +0530

    sweta

---
+++ Pointers/CharacterPointer.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //char ch[10] = "Sweta";
+  //char* c = ch;
+
+  //cout<<ch<<endl; //entire string
+  //cout<<&ch<<endl;
+  //cout<<ch[0]<<endl;
+
+
+  //cout<<&c<<endl;
+  //cout<<*c<<endl; //*(c+0)--> c[0]
+  //cout<<c<<endl;  //entire string
+
+  //char name[9] = "SherBano";
+  // char* c = &name[0];
+
+
+  // cout<<name<<endl;
+  // cout<<&name<<endl;
+  // cout<<*(name + 3)<<endl;
+  // cout<<c<<endl;
+  // cout<<&c<<endl;
+  // cout<<*(c + 3)<<endl;
+  // cout<<c+2<<endl;
+  // cout<<*c<<endl;
+  // cout<<c+8<<endl;
+
+
+  // char ch = 'k';
+  // char* cptr = &ch;
+
+  // cout<<cptr<<endl;
+
+
+
+
+  char name[10] = "Sweta";
+  cout<<name<<endl;
+
+  char* cptr = "sweta";
+  cout<<cptr<<endl;
+
+
+  return 0;
+}
