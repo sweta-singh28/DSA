765a973d1f6437536d2af973a7d8066afee0bb56
sweta-singh28 <swetasinghgs28@gmail.com>
2024-07-07 21:18:32 +0530

    recursion

---
+++ Recursion/RecusionIntro.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int factorial(int n){
+    //base case
+    if(n == 1){
+        return 1;
+    }
+    cout<<"Function is called for n "<<n<<endl;
+    int ans = n * factorial(n-1); //function calling itself
+    return ans;
+}
+
+
+int main() {
+    int n;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+    
+    int ans = factorial(n);
+    cout<<"The ans is: "<<ans<<endl;
+    
+    return 0;
+}
