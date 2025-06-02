fbb7fe3846987c5539223bf63cf5b2fea1a833c3
sweta-singh28 <swetasinghgs28@gmail.com>
2025-06-02 22:27:32 +0530

    questions

--- Arrays/MoveAllZerosToEnd2.cpp
+++ Arrays/MoveAllZerosToEnd2.cpp
@@ -1,12 +1,36 @@
 //maintaining the order of non-zero element
-
 #include<iostream>
 #include<vector>
 using namespace std;
+void pushZerosToEnd(vector<int>& arr) {
+    vector<int>temp;
+    
+    for(int i=0; i<arr.size(); i++){
+        if(arr[i] != 0){
+            temp.push_back(arr[i]);
+        }
+        
+    }
+    
+    
+    for(int i=0; i<temp.size(); i++){
+        arr[i] = temp[i];
+    }
+    
+    
+    for(int i=temp.size(); i<arr.size(); i++){
+        arr[i] = 0;
+    }
+    
+}
 
 int main(){
     vector<int>arr{1,3,5,0,9,1,0,7,0,0};
-    
+
+    pushZerosToEnd(arr);
+    for(int i=0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
     
     
 
